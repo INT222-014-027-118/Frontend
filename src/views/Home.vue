782cc8924f31c15d5efc4f42b33e1acf025216d0
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-09-13 21:52:21 +0700

    update nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,12 @@
         <!-- <div class="relative items-center h-full max-w-6xl mx-auto mb-8 bg-blue-100 rounded-md dark:bg-gray-700" v-for="brand in brandsObjs" :key="brand.brand"> -->
         <div class="container lg:max-w-7xl grid grid-cols-4 gap-1 md:gap-3 mx-auto my-3 md:my-7 md:px-0.5">
             <router-link :to="{ name: 'resultProducts', params: { categoryName: category.itmeName } }" v-for="category in categorys" :key="category">
-                <div class="grid-cols-2 transition bg-white rounded-md shadow-md cursor-pointer md:grid md:h-28 ring ring-transparent hover:ring-primary">
-                    <div class="flex flex-col items-center justify-center p-3 text-center bg-white rounded-md sm:p-5 md:p-7 dark:bg-blue-900 md:bg-gradient-to-r from-secondary to-white">
+                <div class="grid-cols-5 transition bg-white rounded-md shadow-md cursor-pointer md:grid md:h-28 ring ring-transparent hover:ring-primary">
+                    <div class="flex flex-col items-center justify-center col-span-3 p-3 text-center bg-white rounded-md sm:p-5 md:p-7 dark:bg-blue-900 md:bg-gradient-to-r from-secondary to-white">
                         <i class="p-2 mb-2 text-white rounded-full material-icons md:p-3 md:hidden bg-primary md:pointer-events-nonemb-3">{{ category.icon }}</i>
-                        <span class="z-10 text-xs font-semibold tracking-tight text-black uppercase sm:text-base lg:text-xl md:ml-3">{{ category.itmeName }}</span>
+                        <span class="z-10 text-xs font-semibold tracking-wide text-gray-800 uppercase sm:text-base lg:text-xl md:ml-2">{{ category.itmeName }}</span>
                     </div>
-                    <div class="hidden h-full bg-center bg-no-repeat bg-contain md:block rounded-r-md" :style="{ backgroundImage: `url(${category.image})` }"></div>
+                    <div class="hidden h-full col-span-2 bg-center bg-no-repeat bg-contain md:block rounded-r-md" :style="{ backgroundImage: `url(${category.image})` }"></div>
                 </div>
             </router-link>
         </div>
