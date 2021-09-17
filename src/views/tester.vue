fab9f3306a4539a25f51dd05adaf28cd7c01d454
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-09-17 15:08:45 +0700

    Update Rich Select Box

--- src/views/tester.vue
+++ src/views/tester.vue
@@ -6,9 +6,9 @@
                     type="button"
                     class="w-full flex text-left justify-between items-center px-3 py-2 text-black transition duration-100 ease-in-out border rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed bg-white border-gray-300"
                     value="Option A"
-                    @click="showOption = !showOption"
+                    @click="showOP"
                 >
-                    <span class="block truncate ">{{ choosed == "" ? "pleas select" : choosed }}</span
+                    <span class="block truncate" :class="[choosed === '' ? 'text-gray-400' : ' text-black']">{{ choosed == "" ? "please select" : choosed }}</span
                     ><svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current flex-shrink-0 ml-1 h-4 w-4 text-gray-600">
                         <path
                             clip-rule="evenodd"
@@ -22,17 +22,26 @@
                 class="absolute top-12 w-full z-40 -mt-1 border-b border-gray-300 border-l border-r rounded-b shadow-sm transition ease-in duration-75 opacity-100 bg-white"
                 :class="[showOption ? 'visible' : 'invisible']"
             >
-                <div class="inline-block w-full p-2 placeholder-gray-400">
+                <div class="w-full p-2 placeholder-gray-400 flex items-center">
                     <input
                         placeholder="Search..."
-                        class="inline-block w-full px-3 py-2 bg-gray-50 text-sm rounded border focus:outline-none focus:shadow-outline border-gray-300"
+                        class="inline w-full px-3 py-2 h-9 bg-gray-50 text-sm rounded-md border focus:outline-none focus:shadow-outline border-gray-300"
                         v-model="text"
-                        @keydown.enter="search"
+                        ref="inputText"
                     />
+                    <div class="" v-show="showAdd">
+                        <button class="bg-green-600 px-2 py-2 ml-2 h-9 rounded-md text-white text-sm" @click="AddOption">Add</button>
+                    </div>
                 </div>
                 <ul class="overflow-auto " style="max-height: 300px;">
-                    <li data-type="option" class="cursor-pointer" :class="[check ? 'font-semibold bg-blue-600  text-white' : '']">
-                        <div class="flex justify-between items-center px-3 py-2" v-for="(spec, index) in search" :key="spec" @click="selectToNa(index)" :class="{ hidden: !spec.show }">
+                    <li
+                        data-type="option"
+                        class="cursor-pointer hover:bg-blue-100"
+                        v-for="(spec, index) in search"
+                        :key="spec"
+                        :class="[spec.active ? 'font-semibold bg-blue-600 text-white hover:bg-blue-700' : '']"
+                    >
+                        <div class="flex justify-between items-center px-3 py-2" @click="selectToNa(index)" v-if="spec.show">
                             <span class="truncate block ">{{ spec.spec }}</span>
                             <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current h-4 w-4" :class="{ hidden: !spec.active }">
                                 <polygon points="0 11 2 9 7 14 18 3 20 5 7 18"></polygon>
@@ -51,6 +60,7 @@ export default {
     data() {
         return {
             showOption: false,
+            showAdd: false,
             specs: [
                 { spec: "spec1", active: false, show: true },
                 { spec: "spec2", active: false, show: true },
@@ -58,7 +68,6 @@ export default {
             ],
             choosed: "",
             text: "",
-            check: Boolean,
         };
     },
     methods: {
@@ -73,8 +82,17 @@ export default {
             }
             this.showOption = false;
         },
-        newObj(arr) {
-            this.specs = arr;
+        showOP() {
+            this.showOption = !this.showOption;
+            setTimeout(() => {
+                this.$refs.inputText.focus();
+            }, 500);
+        },
+        AddOption() {
+            if (!this.text == "" && !this.text == this.specs.filter((t) => t.spec.toLowerCase().includes(this.text.toLowerCase())) ) {
+                let newOp = { spec: this.text, active: false, show: true };
+                this.specs.push(newOp);
+            }
         },
     },
     computed: {
@@ -82,8 +100,10 @@ export default {
             this.specs.map((t) => {
                 if (!t.spec.toLowerCase().includes(this.text.toLowerCase())) {
                     t.show = false;
+                    this.showAdd = true;
                 } else {
                     t.show = true;
+                    this.showAdd = false;
                 }
             });
             return this.specs;
